10a8ae5ca3330949c8bf6d0734dbdc28840cfb6f
fernando cibau <fernandocibau89@gmail.com>
2024-03-24 13:36:01 -0300

    ARREGLOS EN LAS FOTOS Y CARRUOSEL

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 //                          FUNCIONES
 //--------------------------------------------------------------------------
 //----- AUDIO 
-function audioPlay(){
-    if( audio.classList.contains("play") ){
+function audioPlay() {
+    if (audio.classList.contains("play")) {
         audio.classList.remove("play");
         audio.src = " ";
-    }else{
+    } else {
         audio.classList.add("play");
         audio.src = "./audio/Hannah Montana The Movie  The Climb scena dal film.mp3";
     }
@@ -18,21 +18,77 @@ function audioPlay(){
 //---------------------------------------------------------------------------
 //                           INICIO HTML
 //---------------------------------------------------------------------------
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
     audioPlay();
 });
 
 //-----------------------------------------------------------------------------
-//                      BOTONES
+//                      BOTONES AUDIO
 // ----------------------------------------------------------------------------
-//----- AUDIO 
 let miBoton = document.getElementById("play-pause-music");
 let audio = document.getElementById("audio");
 
-miBoton.addEventListener("click", ()=> {
-        audioPlay();
+miBoton.addEventListener("click", () => {
+    audioPlay();
 
+})
+//-----------------------------------------------------------------------------
+//                      CARROUSEL
+// ----------------------------------------------------------------------------
+window.addEventListener("load", () => {
+    new Glider(document.querySelector('.list_carrousel'), {
+        slidesToScroll: 1,
+        slidesToShow: 2,
+        draggable: true,
+        dots: '.indice_carrousel',
+        
+        arrows: {
+            prev: '.prev',
+            next: '.next'
+        },
+
+        responsive: [
+            {
+                // screens greater than >= 775px
+                breakpoint: 300,
+                settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    draggable: true,
+                    // arrows: {
+                    //     prev: '.prev',
+                    //     next: '.next'
+                    // },
+                    // itemWidth: 150,
+                    // duration: 0.25
+                }
+            }, {
+                // screens greater than >= 1024px
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    draggable: true,
+                    // arrows: {
+                    //     prev: '.prev',
+                    //     next: '.next'
+                    // },
+                    // // itemWidth: 150,
+                    // duration: 0.25
+                }
+            }
+        ]
+        ////-----------------------------
+
+    });
 })
 
 
-//------------------------------------------------------------------------
+
+// <a href="https://www.fixdate.io/modelo-invitacion/36/img/galeria/1.jpg"
+// data-fancybox="galeria" tabindex="-1">
+// <img src="https://www.fixdate.io/modelo-invitacion/36/img/galeria/1.jpg" alt="">
+// </a>
+
+//---------------------------------------------------------------------------
